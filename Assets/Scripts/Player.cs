7bf3ca25df5ab7dddbc21e610714d1ece05609dd
İlkay Özer <ilkayyozerr@gmail.com>
2022-12-27 15:56:29 +0300

    jump_Debugged

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Tilemaps;
@@ -12,6 +13,8 @@ public class Player : Creature
     public LayerMask jumpableGround;
 
     public float jumpForce = 1000f;
+
+    private bool canDash;
 
     void Start()
     {
@@ -30,11 +33,20 @@ public class Player : Creature
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
+        rb.velocity = new Vector2(dir.x * movementSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && IsGrounded())
+        {
+            //StartCoroutine(Dash());
+        }
+
+        updateAnim.UpdateAnimation(dir);
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + dir * movementSpeed * Time.fixedDeltaTime);
+        //rb.MovePosition(rb.position + dir * movementSpeed * Time.fixedDeltaTime);
     }
 
     private bool IsGrounded()
@@ -42,6 +54,23 @@ public class Player : Creature
         return Physics2D.BoxCast(boxcol.bounds.center, boxcol.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
     }
 
-
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        if (transform.rotation.eulerAngles.y == 180f)
+        {
+            rb.velocity = new Vector2(-dashForce, 0f);
+        }
+        else
+        {
+            rb.velocity = new Vector2(dashForce, 0f);
+        }
+        anim.Play("Dash");
+        yield return new WaitForSeconds(dashTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
 
 }
