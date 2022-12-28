662f82ac89e9aa27a8e3d2d5521b677b6bc3dbdf
İlkay Özer <ilkayyozerr@gmail.com>
2022-12-28 04:47:21 +0300

    boss_Added

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : Creature
+{
+    public float bossMovementSpeed;
+
+    public Transform playerTransform;
+    private float dirEnemy;
+
+    private HealthBar healthBar;
+    private int maxHealth = 100;
+    private int hp;
+
+    public GameObject ability;
+    private bool canAttack = true;
+    private bool isAttacking = false;
+    private float attackTime = 1f;
+
+    private enum MovementState { idle, running }
+    private MovementState state;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        hp = maxHealth;
+        //healthBar.SetMaxHealth(maxHealth);
+    }
+
+
+    void Update()
+    {
+        if (isDead)
+        {
+            return;
+        }
+
+        dirEnemy = -playerTransform.position.x + transform.position.x;
+
+        if (isAttacking)
+        {
+            return;
+        }
+
+        if (dirEnemy < 1f && dirEnemy > -1f)
+        {
+            state = MovementState.idle;
+            rb.velocity = new Vector2(0f, 0f);
+            if (canAttack)
+            {
+                StartCoroutine(Attack());
+            }
+        }
+
+        else if (dirEnemy < 10f && dirEnemy > -10f)
+        {
+            if (dirEnemy > 0f)
+            {
+                rb.velocity = new Vector2(-bossMovementSpeed, rb.velocity.y);
+                transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                state = MovementState.running;
+            }
+            else if (dirEnemy < 0f)
+            {
+                rb.velocity = new Vector2(bossMovementSpeed, rb.velocity.y);
+                transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+                state = MovementState.running;
+            }
+        }
+        else
+        {
+            state = MovementState.idle;
+        }
+
+
+        anim.SetInteger("state", (int)state);
+
+    }
+
+    private IEnumerator Attack()
+    {
+
+        if (!isHitting)
+        {
+            canAttack = false;
+            isAttacking = true;
+            anim.Play("Attack");
+            yield return new WaitForSeconds(0.4f);
+            ability.GetComponent<EnemyAttack>().Attack();
+            yield return new WaitForSeconds(0.7f);
+            isAttacking = false;
+            canAttack = true;
+        }
+    }
+}
